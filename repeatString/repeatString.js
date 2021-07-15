377d3d0f9c95750f50260032060c723deb84b09a
Andres Torres <magoyde@gmail.com>
2021-07-15 00:57:33 -0500

    fundamentals part 4

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    if (times < 0) return 'ERROR';
+    let res = '';
+    for (let i=0; i<times; i++){
+        res += str;
+    }
+    return res;
 };
 
 module.exports = repeatString;
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverse = '';
+    for (let i=(str.length-1); i >= 0; i--){
+        reverse = reverse + str[i];
+    }
+  return reverse;
 };
 
 module.exports = reverseString;
