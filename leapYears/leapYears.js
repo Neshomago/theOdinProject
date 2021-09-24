6d21bdd008c32bb790142dd6a3a55718a009bf5d
Andres Torres <magoyde@gmail.com>
2021-09-24 01:04:41 -0500

    Not working the logic yet

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+ if (year%4 === 0){
+     console.log("block divided by four", true);
+ } else if ( year%400 === 0){
+    console.log("block divided by 400", true);
+ } else {
+     console.log("todo falso como la buena voluntad de carola");
+ }
 };
 
 module.exports = leapYears;
