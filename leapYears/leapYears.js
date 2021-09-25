7f7416f1a1bd512ff334fd4831f35646abd868bd
Andres Torres <magoyde@gmail.com>
2021-09-24 21:11:54 -0500

    Leap years done

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,10 +1,8 @@
 const leapYears = function(year) {
- if (year%4 === 0){
-     console.log("block divided by four", true);
- } else if ( year%400 === 0){
-    console.log("block divided by 400", true);
- } else {
-     console.log("todo falso como la buena voluntad de carola");
+ if ((year%4 === 0) && (year%100 !== 0) || year%400 === 0){
+     return true;
+ } else{
+     return false;
  }
 };
 
